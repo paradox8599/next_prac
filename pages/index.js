4816e457e0f6d4da9a984e1f19a30019ff4a46e1
paradox8599 <paradox8599@outlook.com>
2023-04-10 23:03:00 +1000

    data fetching

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,67 @@
-import Head from 'next/head';
-import Layout, { siteTitle } from '../components/layout';
-import utilStyles from '../styles/utils.module.css';
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import {
+  GetStaticProps,
+  GetStaticPaths,
+  GetServerSideProps,
+  GetServerSidePropsContext,
+} from "next";
 
-export default function Home() {
+// export async function getStaticProps() {
+//   const allPostsData = getSortedPostsData();
+//   return {
+//     props: {
+//       allPostsData,
+//     },
+//   };
+// }
+
+/**
+ * @param {GetServerSidePropsContext}
+ * @returns {GetServerSideProps}
+ */
+export async function getServerSideProps({ req, res }) {
+  const allPostsData = getSortedPostsData();
+  // const myip = await fetch("https://api.myip.com/");
+  // const myipjson = await myip.json();
+
+  return {
+    props: {
+      allPostsData,
+      // myipjson,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
   return (
     <Layout home>
       <Head>
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
+        {/* <p>IP: {myipjson.ip}</p>
+        <p>Country: {myipjson.country} {myipjson.cc}</p> */}
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
+          (This is a sample website - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              {title}
+              <br />
+              {id}
+              <br />
+              {date}
+            </li>
+          ))}
+        </ul>
       </section>
     </Layout>
   );
